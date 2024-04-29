d63dbbcbcba9e2f726ec6b33cbc6ed8f7d5c914f
mnolimp <37767648+mnolimp@users.noreply.github.com>
2024-04-29 11:47:09 +0600

    Added list of cities
    Added clickable list of cities
    Changed all icons to size 800x800

--- WeatherForecastProvider/app/src/main/java/com/mnolimp/weatherforecastprovider/MainActivity.kt
+++ WeatherForecastProvider/app/src/main/java/com/mnolimp/weatherforecastprovider/MainActivity.kt
@@ -6,10 +6,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
@@ -22,6 +26,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 import org.json.JSONObject
 import java.net.URL
 import java.util.concurrent.Executors
@@ -35,6 +40,14 @@ private var weatherMinTemperature : String = "Zero"
 private var weatherType : String = "Unknown"
 private var weatherCloudness : Int = 0
 private var weatherWindSpeed : Int = 0
+private val citiesList = arrayListOf(
+    arrayListOf("Omsk", "55.00", "73.24"),
+    arrayListOf("Moscow", "55.45", "37.37"),
+    arrayListOf("Saint-Petersburg", "59.57", "30.19"),
+    arrayListOf("Novosibirsk", "55.01", "82.55"),
+    arrayListOf("Ekaterinburg", "56.50", "60.35"),
+    arrayListOf("Vladivostok", "43.07", "131.54")
+)
 
 class MainActivity : ComponentActivity() {
 
@@ -46,17 +59,18 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-fun APICall(stateLocation: MutableState<String>,
-            stateWeatherTemperature: MutableState<String>,
-            stateWeatherFeelsLike: MutableState<String>,
-            stateWeatherMaxTemperature: MutableState<String>,
-            stateWeatherMinTemperature: MutableState<String>,
-            stateWeatherType: MutableState<String>
+fun APICall(
+    stateLocation: MutableState<String>,
+    stateWeatherTemperature: MutableState<String>,
+    stateWeatherFeelsLike: MutableState<String>,
+    stateWeatherMaxTemperature: MutableState<String>,
+    stateWeatherMinTemperature: MutableState<String>,
+    stateWeatherType: MutableState<String>,
+    lat : String,
+    lon : String
 ) {
     executor.execute {
         val APIkey = "c2ce0be54eeb289483acdd10c56d282b"
-        val lat = "44.34"
-        val lon = "10.99"
 
         val url =
             "https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&appid=$APIkey&units=metric"
@@ -118,15 +132,6 @@ fun showInfo() {
     var icTypeId = remember{
         mutableStateOf(0)
     }
-
-    APICall(
-        stateLocation,
-        stateWeatherTemperature,
-        stateWeatherFeelsLike,
-        stateWeatherMaxTemperature,
-        stateWeatherMinTemperature,
-        stateWeatherType
-    )
 
     when(weatherType){
         "Clear" -> icTypeId.value = R.drawable.ic_sunny
@@ -198,6 +203,30 @@ fun showInfo() {
                     fontSize = 15.sp,
                     textAlign = TextAlign.Left
                 )
+            }
+        }
+        LazyColumn(modifier = Modifier.fillMaxHeight()) {
+            for(i in 0..citiesList.size-1){
+                item {
+                    Row(horizontalArrangement = Arrangement.Center, modifier = Modifier.fillMaxWidth().clickable {
+                        APICall(
+                            stateLocation,
+                            stateWeatherTemperature,
+                            stateWeatherFeelsLike,
+                            stateWeatherMaxTemperature,
+                            stateWeatherMinTemperature,
+                            stateWeatherType,
+                            citiesList.get(i).get(1),
+                            citiesList.get(i).get(2)
+                        )
+                    }) {
+                        Text(
+                            text = citiesList.get(i).get(0),
+                            fontSize = 22.sp,
+                            textAlign = TextAlign.Center
+                        )
+                    }
+                }
             }
         }
     }
